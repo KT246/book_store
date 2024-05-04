dd5f8f465ba6cc8e4df8495e9525ffb75e0bb2a1
KT246 <tayattapeu@gmail.com>
2024-05-04 17:32:43 +0700

    second

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    
+    include_once 'component/navbar.php';
+    // include_once 'component/header.php';
+    // include_once 'component/menu.php';
+    // include_once 'component/content.php';
+    // include_once 'component/footer.php';
+
+?>
